57354e35195d33b945c5da2423e69fd76783fb8b
MichaelJahns <jahnsmic001@gmail.com>
2021-02-28 15:44:15 -0800

    Revamps and condenses timeofday.js into what seems to be essential code

---
+++ timeofday.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let greetings = [
+	'Night',
+	'Early Morning',
+	'Morning',
+	'Afternoon',
+	'Evening',
+]
+// global vars
+var lastTimeTag;
+
+export function update() {
+	let hour = new Date().getHours();
+	let timeTag = getTimeTag(hour)
+	console.log(hour, timeTag);
+
+	// Why are we keeping a reference for past time tags? How often is update called?
+	if (lastTimeTag != timeTag) {
+		lastTimeTag = timeTag
+		thisLayer.text = newString.replace('$', engine.userProperties.name);
+	}
+}
+
+function getTimeTag(hour){
+	switch (hour) {
+		case hour < 5 :
+		timeTag = greetings[0]
+		break;
+
+		case hour < 8 :
+		timeTag = greetings[1]
+		break;
+
+		case hour < 12:
+		timeTag = greetings[2];
+		break;
+
+		case hour < 18 :
+		timeTag = greetings[3];
+		break;
+
+		case hour < 22 :
+		timeTag = greetings[4]
+		break;
+		
+		case hour > 22 :
+		timeTag = greetings[0]
+		break;
+	}
+}
+
+update()
